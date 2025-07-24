accef677b08484d5c81b3330593f53332e925f49
Benhayoun M hamed <mr.benhayoun@gmail.com>
2025-07-24 15:09:56 +0200

    Initial commit: Spring Boot setup

---
+++ src/main/java/com/example/WarehouseManager96/WarehouseManager96Application.java
@@ -0,0 +1,13 @@
+package com.example.WarehouseManager96;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManager96Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManager96Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WarehouseManager96/WarehouseManager96ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WarehouseManager96;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManager96ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
