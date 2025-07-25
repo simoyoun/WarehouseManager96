3c7d080c213b130e7c57dd0f3ba50f0752ed411c
Benhayoun M hamed <mr.benhayoun@gmail.com>
2025-07-25 23:44:39 +0200

    scan done

---
+++ src/main/java/com/example/WarehouseManager96/InventoryItem.java
@@ -0,0 +1,4 @@
+package com.example.WarehouseManager96;
+
+public class InventoryItem {
+}
---
+++ src/main/java/com/example/WarehouseManager96/repository/InventoryRepository.java
@@ -0,0 +1,4 @@
+package com.example.WarehouseManager96.repository;
+
+public class InventoryRepository {
+}
